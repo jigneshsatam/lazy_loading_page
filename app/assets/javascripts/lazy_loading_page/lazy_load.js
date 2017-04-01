a54e1506410b9c9ec41ee67c1df9a8a259b6c693
Jignesh Satam <jigneshsatam@Jigneshs-MacBook-Pro.local>
2017-04-02 00:19:53 +0530

    Added support for javascript requests

--- app/assets/javascripts/lazy_loading_page/lazy_load.js
+++ app/assets/javascripts/lazy_loading_page/lazy_load.js
@@ -8,16 +8,11 @@
 });
 
 function lazyLoad(){
-  var $lazy_loaders = document.querySelectorAll(".lazy_load:not([data-later='true'])");
+  var $lazy_loaders = document.querySelectorAll(".lazy_load:not([data-later='true']):not([data-loading='started'])");
   for (var i = 0; i < $lazy_loaders.length; i++) {
     (function(i){
       $lazy_loader = $lazy_loaders[i];
       loadElement($lazy_loader);
-      // xhttp.setRequestHeader("Content-type", "application/js");
-      // $.ajax({
-      //  $lazy_loader.getAttribute("data-id")
-      //  url = $lazy_loader.getAttribute("data-url")
-      // });
     })(i);
   };
 }
@@ -33,19 +28,21 @@ function loadElement(ele){
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function(){ajaxCallback(xhttp, id);};
   xhttp.open("GET", url, true);
-  xhttp.overrideMimeType('application/javascript');
-  // xhttp.responseType = "javascript";
-  // xhttp.setRequestHeader("Content-type", "application/javascript");
+  if (ele.getAttribute("data-type") == "script")
+    xhttp.setRequestHeader('Accept', 'text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*');
+  // xhttp.setRequestHeader("X-CSRF-Token", document.querySelector("[name='csrf-token']").content);
+  xhttp.setRequestHeader("X-Requested-With", "XMLHttpRequest");
   xhttp.send();
+  ele.setAttribute("data-loading", "started");
 }
 
 function addLoader(ele){
-  // var loader_img = $("<div class='"+id+"' style='height: 40px; width: 100%;'><div class='uil-ellipsis-css'><div class='ib'><div class='circle'><div></div></div><div class='circle'><div></div></div><div class='circle'><div></div></div><div class='circle'><div></div></div><div class='clear'></div></div></div></div>")
-  // var loader_img = $('<div class="loading-container" data-id="'+id+'"><span class="loading-indicator"><i></i><i></i><i></i></span></div>');
-  // $($lazy_loader).parent().append(loader_img);
   var loader_img = document.createElement("div");
   loader_img.setAttribute("class", "loading-container")
   loader_img.setAttribute("data-id", ele.getAttribute("data-id"));
+  if (["script", "json"].indexOf(ele.getAttribute("data-type")) >= 0)
+    loader_img.setAttribute("data-type", ele.getAttribute("data-type"));
+
   var span = document.createElement("span");
   span.setAttribute("class", "loading-indicator")
   for(var j=0; j<3; j++){
@@ -55,46 +52,62 @@ function addLoader(ele){
   loader_img.appendChild(span)
   ele.parentElement.replaceChild(loader_img, ele);
   loader_img.appendChild(ele);
-  // var loader_img = $('<div class="loading-container" data-id="'+ele.getAttribute("data-id")+'"><span class="loading-indicator"><i></i><i></i><i></i></span></div>');
-  // $(ele).replaceWith(loader_img);
-  // loader_img.append($(ele));
 }
 
 function ajaxCallback(xhttp, id) {
-  var elementToReplace = document.querySelectorAll("[data-id='"+id+"']")[0];
   if (xhttp.readyState == 4 && xhttp.status == 200) {
-    var parser = new DOMParser();
-    var doc = parser.parseFromString(xhttp.responseText, "text/html");
-    var newElements = doc.querySelector("body");
-    if (window.$ !== undefined){
-    // if (false){
-      new_ele = $(xhttp.responseText);
-      if (newElements !== null){
-        new_ele = $(newElements.innerHTML);
-      }
-      $(elementToReplace).replaceWith(new_ele);
-      $("."+ id).remove();
-    }
-    else{
-      var parentElement = elementToReplace.parentNode;
-      var template = document.createElement("template");
-      if (newElements !== null){
-        template.innerHTML = newElements.innerHTML;
-      }
-      else{
-        template.innerHTML = xhttp.responseText;
-      }
-      var childNodes = template.content.childNodes
-      var newElement = elementToReplace
-      for(var i= childNodes.length-1; i>=0 ; i--){
-        newPreviousElement = childNodes[i];
-        parentElement.insertBefore(newPreviousElement, newElement);
-        if(newPreviousElement.nodeName == "SCRIPT"){
-          eval(newPreviousElement.textContent);
-        }
-        newElement = newPreviousElement;
-      }
+    var elementToReplace = document.querySelectorAll("[data-id='"+id+"']")[0];
+    var parentElement = elementToReplace.parentNode;
+    if (elementToReplace.getAttribute("data-type") == "script"){
+    // if(true){
+      newScript = document.createElement("script");
+      newScript.innerHTML = xhttp.responseText;
+      parentElement.insertBefore(newScript, elementToReplace);
       parentElement.removeChild(elementToReplace);
     }
+    else{
+      var parser = new DOMParser();
+      var doc = parser.parseFromString(xhttp.responseText, "text/html");
+      var newElements = doc.querySelector("body");
+      if (window.$ !== undefined){
+      // if (false){
+        new_ele = $(xhttp.responseText);
+        if (newElements !== null){
+          new_ele = $(newElements.innerHTML);
+        }
+        $(elementToReplace).replaceWith(new_ele);
+        $("."+ id).remove();
+      }
+      else{
+        var template = document.createElement("template");
+        if (newElements !== null){
+          template.innerHTML = newElements.innerHTML;
+        }
+        else{
+          template.innerHTML = xhttp.responseText;
+        }
+        var childNodes = template.content.childNodes
+        var newElement = elementToReplace
+        var scriptTags = [];
+        for(var i= childNodes.length-1; i>=0 ; i--){
+          newPreviousElement = childNodes[i];
+          parentElement.insertBefore(newPreviousElement, newElement);
+          if(newPreviousElement.nodeName == "SCRIPT"){
+            scriptTags.push(newPreviousElement.textContent);
+            eval();
+          }
+          newElement = newPreviousElement;
+        }
+        runScripts(scriptTags);
+        parentElement.removeChild(elementToReplace);
+      }
+    }
+    lazyLoad();
   }
+}
+
+function runScripts(scriptTags){
+  scriptTags.forEach( function(scriptTag){
+    eval(scriptTag)
+  });
 }
--- app/helpers/lazy_loading_page/application_helper.rb
+++ app/helpers/lazy_loading_page/application_helper.rb
@@ -6,8 +6,9 @@ module LazyLoadingPage
       classes = ("lazy_load " + options[:class] rescue "lazy_load")
       params = options[:params]
       data_merge_hash = {}
-      options[:later].present?  ? (options[:later].to_s.downcase == "true" ? data_merge_hash[:later] = true : false) : false
-      options[:loader].present? ? (options[:loader].to_s.downcase == "off" ? data_merge_hash[:loader] = false : true) : true
+      options[:later].present? && (options[:later].to_s.downcase == "true") && (data_merge_hash[:later] = true)
+      options[:loader].present? && (options[:loader].to_s.downcase == "off") && (data_merge_hash[:loader] = false)
+      options[:type].present? && (["script", "json"].include?(options[:type])) && (data_merge_hash[:type] = options[:type])
       content_tag("input", nil, type:"hidden", id: id, class: classes, data: {id: SecureRandom.uuid, url: url}.merge!(data_merge_hash))
     end
   end
