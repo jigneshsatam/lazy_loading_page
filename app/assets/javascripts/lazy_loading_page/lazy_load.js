b70fa291982bc636c58822eb2f834fbb49f9ebc0
Jignesh Satam <jigneshsatam@Jigneshs-MacBook-Pro.local>
2017-04-02 22:36:39 +0530

    Refactored code and made it modularized

--- app/assets/javascripts/lazy_loading_page/lazy_load.js
+++ app/assets/javascripts/lazy_loading_page/lazy_load.js
@@ -2,8 +2,8 @@
   if (window.lazyLoaderInitialize === undefined) {
     document.addEventListener(e, function() {
       lazyLoad();
-    })
-    window.lazyLoaderInitialize = true
+    });
+    window.lazyLoaderInitialize = true;
   }
 });
 
@@ -22,8 +22,7 @@ function loadElement(ele){
     ele = ele[0];
   var id = ele.getAttribute("data-id");
   var url = ele.getAttribute("data-url");
-  if (ele.getAttribute("data-loader")
-    != "false")
+  if (ele.getAttribute("data-loader") != "false")
     addLoader(ele);
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function(){ajaxCallback(xhttp, id);};
@@ -38,72 +37,97 @@ function loadElement(ele){
 
 function addLoader(ele){
   var loader_img = document.createElement("div");
-  loader_img.setAttribute("class", "loading-container")
+  loader_img.setAttribute("class", "loading-container");
   loader_img.setAttribute("data-id", ele.getAttribute("data-id"));
   if (["script", "json"].indexOf(ele.getAttribute("data-type")) >= 0)
     loader_img.setAttribute("data-type", ele.getAttribute("data-type"));
 
   var span = document.createElement("span");
-  span.setAttribute("class", "loading-indicator")
+  span.setAttribute("class", "loading-indicator");
   for(var j=0; j<3; j++){
     var i = document.createElement("i");
     span.appendChild(i);
   }
-  loader_img.appendChild(span)
+  loader_img.appendChild(span);
   ele.parentElement.replaceChild(loader_img, ele);
   loader_img.appendChild(ele);
 }
 
 function ajaxCallback(xhttp, id) {
-  if (xhttp.readyState == 4 && xhttp.status == 200) {
-    var elementToReplace = document.querySelectorAll("[data-id='"+id+"']")[0];
-    var parentElement = elementToReplace.parentNode;
-    if (elementToReplace.getAttribute("data-type") == "script"){
-    // if(true){
-      newScript = document.createElement("script");
-      newScript.innerHTML = xhttp.responseText;
-      parentElement.insertBefore(newScript, elementToReplace);
-      parentElement.removeChild(elementToReplace);
+  if (xhttp.readyState == XMLHttpRequest.DONE){
+    requestComplete(xhttp, id);
+    // Call complete Callback
+  }
+}
+
+function requestComplete(xhttp, id){
+  if (xhttp.status == 200) {
+    requestSuccess(xhttp, id);
+    // Call success Callback
+  }
+  else{
+    // Falilure Code
+    // requestFailure(xhttp, id)
+    // Call failure Callback
+  }
+}
+
+function requestSuccess(xhttp, id){
+  var elementToReplace = document.querySelectorAll("[data-id='"+id+"']")[0];
+  var parentElement = elementToReplace.parentNode;
+  if (elementToReplace.getAttribute("data-type") == "script"){
+  // if(true){
+    javascriptResponseActions(xhttp, parentElement, elementToReplace);
+  }
+  else{
+    var parser = new DOMParser();
+    var doc = parser.parseFromString(xhttp.responseText, "text/html");
+    var newElements = doc.querySelector("body");
+    if (window.$ !== undefined){
+    // if (false){
+      new_ele = $(xhttp.responseText);
+      if (newElements !== null){
+        new_ele = $(newElements.innerHTML);
+      }
+      $(elementToReplace).replaceWith(new_ele);
+      $("."+ id).remove();
     }
     else{
-      var parser = new DOMParser();
-      var doc = parser.parseFromString(xhttp.responseText, "text/html");
-      var newElements = doc.querySelector("body");
-      if (window.$ !== undefined){
-      // if (false){
-        new_ele = $(xhttp.responseText);
-        if (newElements !== null){
-          new_ele = $(newElements.innerHTML);
-        }
-        $(elementToReplace).replaceWith(new_ele);
-        $("."+ id).remove();
+      var template = document.createElement("template");
+      if (newElements !== null){
+        template.innerHTML = newElements.innerHTML;
       }
       else{
-        var template = document.createElement("template");
-        if (newElements !== null){
-          template.innerHTML = newElements.innerHTML;
-        }
-        else{
-          template.innerHTML = xhttp.responseText;
-        }
-        var childNodes = template.content.childNodes
-        var newElement = elementToReplace
-        var scriptTags = [];
-        for(var i= childNodes.length-1; i>=0 ; i--){
-          newPreviousElement = childNodes[i];
-          parentElement.insertBefore(newPreviousElement, newElement);
-          if(newPreviousElement.nodeName == "SCRIPT"){
-            scriptTags.push(newPreviousElement.textContent);
-            eval();
-          }
-          newElement = newPreviousElement;
-        }
-        runScripts(scriptTags);
-        parentElement.removeChild(elementToReplace);
+        template.innerHTML = xhttp.responseText;
       }
+      var childNodes = template.content.childNodes
+      var newElement = elementToReplace
+      var scriptTags = [];
+      for(var i= childNodes.length-1; i>=0 ; i--){
+        newPreviousElement = childNodes[i];
+        parentElement.insertBefore(newPreviousElement, newElement);
+        if(newPreviousElement.nodeName == "SCRIPT"){
+          scriptTags.push(newPreviousElement.textContent);
+          eval();
+        }
+        newElement = newPreviousElement;
+      }
+      runScripts(scriptTags);
+      parentElement.removeChild(elementToReplace);
     }
-    lazyLoad();
   }
+  lazyLoad();
+}
+
+// function requestFailure(xhttp, id){
+
+// }
+
+function javascriptResponseActions(xhttp, parentElement, elementToReplace){
+  var newScript = document.createElement("script");
+  newScript.innerHTML = xhttp.responseText;
+  parentElement.insertBefore(newScript, elementToReplace);
+  parentElement.removeChild(elementToReplace);
 }
 
 function runScripts(scriptTags){
